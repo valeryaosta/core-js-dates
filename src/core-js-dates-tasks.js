77c3d21e022325904b40a79d5f1d303308b52195
valeryaosta <gerasimenko.valery@gmail.com>
2024-03-13 23:03:19 +0300

    feat: add solution for the task getCountWeekendsInMonth

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -185,8 +185,19 @@ function formatDate(date) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  let totalWeekends = 0;
+  const date = new Date(year, month - 1, 1);
+
+  while (date.getMonth() === month - 1) {
+    const dayOfWeek = date.getDay();
+    if (dayOfWeek === 0 || dayOfWeek === 6) {
+      totalWeekends += 1;
+    }
+    date.setDate(date.getDate() + 1);
+  }
+
+  return totalWeekends;
 }
 
 /**
